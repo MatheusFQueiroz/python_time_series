ae2904c693cdd9ddd8ecc39adbf33817c84e9d69
Matheus <mf5881585@gmail.com>
2025-08-25 20:30:40 -0300

    cabecalhos x docstring

---
+++ cadeira_preto.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# author: <nome>
+# date: <data>
+# description: <desc>
+# license: <license>
+
+"""
+    docstring
+"""
+
+# Cabeçalhos: são linhas de comentários do topo do arquivo, usado para descrever e fornecer informações para o desenvolvedor.
+# Não é extritamente necessários, mas é uma boa prática utilizar.
+
+"""
+    Docstrings são uma forma de documentação incorporada ao código, elas são colocadas logo após a definição de funções,
+    métodos ou classes, e servem para descrever o que eles fazem.
+"""
