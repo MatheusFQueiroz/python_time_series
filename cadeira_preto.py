c0f8c694569cfe3bfaa061249b6e2e99ff566857
Matheus <mf5881585@gmail.com>
2025-08-28 03:27:19 -0300

    joinha

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -22,14 +22,44 @@
 def e_par(n: int) -> bool:
     """Retorna True se n é par, senão False."""
     # TODO: retorne se n é par
+    """
+    if n % 2 == 0 :
+        return true
+    else:
+        return false
+    """
+    return n % 2 == 0
     ...
 
 def fatorial(n: int) -> int:
     """Retorna n! (n fatorial). Para n<0, levante ValueError."""
     # TODO: implemente de forma iterativa (sem recursão)
+    aux = 1
+    for i in range(1, n) :
+        aux = aux * (i + 1)
+
+    return aux
     ...
 
 def maximo(nums):
     """Retorna o maior elemento de uma lista não vazia, sem usar max()."""
     # TODO: percorra a lista guardando o maior atual
+    try:
+        if len(nums) == 0 :
+            raise ValueError
+    except ValueError:
+        print('An empty array was provided.')
+    else:
+        aux = nums[0]
+        for i in range(0, len(nums)):
+            if aux < nums[i] :
+                aux = nums[i]
+        
+        return aux
     ...
+
+
+print(e_par(3))
+print(fatorial(5))
+print(maximo([12, 0, 17, 28, 39]))
+maximo([])
